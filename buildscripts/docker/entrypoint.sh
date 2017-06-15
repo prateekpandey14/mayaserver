07892d434ecf6009752baeb186cf2cd8a8981747
Kiran Mova <kiranmova@gmail.com>
2017-06-15 22:43:37 +0530

    template already filled with values. avoid sed

--- buildscripts/docker/entrypoint.sh
+++ buildscripts/docker/entrypoint.sh
@@ -16,16 +16,16 @@ CONTAINER_IP_ADDR=$(ip -4 addr show scope global dev "${MAYA_API_SERVER_NETWORK}
 # Here nomad running on global region's config file is considered
 
 # nomad address related
-sed -i "s/__NOMAD_ADDR__/${NOMAD_ADDR}/g" /etc/mayaserver/orchprovider/nomad_global.INI
+#sed -i "s/__NOMAD_ADDR__/${NOMAD_ADDR}/g" /etc/mayaserver/orchprovider/nomad_global.INI
 
 # cn related
-sed -i "s/__CN_TYPE__/${NOMAD_CN_TYPE}/g" /etc/mayaserver/orchprovider/nomad_global.INI
-sed -i "s/__CN_NETWORK_CIDR__/${NOMAD_CN_NETWORK_CIDR}/g" /etc/mayaserver/orchprovider/nomad_global.INI
-sed -i "s/__CN_INTERFACE__/${NOMAD_CN_INTERFACE}/g" /etc/mayaserver/orchprovider/nomad_global.INI
+#sed -i "s/__CN_TYPE__/${NOMAD_CN_TYPE}/g" /etc/mayaserver/orchprovider/nomad_global.INI
+#sed -i "s/__CN_NETWORK_CIDR__/${NOMAD_CN_NETWORK_CIDR}/g" /etc/mayaserver/orchprovider/nomad_global.INI
+#sed -i "s/__CN_INTERFACE__/${NOMAD_CN_INTERFACE}/g" /etc/mayaserver/orchprovider/nomad_global.INI
 
 # cs related
-sed -i "s/__CS_PERSISTENCE_LOCATION__/${NOMAD_CS_PERSISTENCE_LOCATION}/g" /etc/mayaserver/orchprovider/nomad_global.INI
-sed -i "s/__CS_REPLICA_COUNT__/${NOMAD_CS_REPLICA_COUNT}/g" /etc/mayaserver/orchprovider/nomad_global.INI
+#sed -i "s/__CS_PERSISTENCE_LOCATION__/${NOMAD_CS_PERSISTENCE_LOCATION}/g" /etc/mayaserver/orchprovider/nomad_global.INI
+#sed -i "s/__CS_REPLICA_COUNT__/${NOMAD_CS_REPLICA_COUNT}/g" /etc/mayaserver/orchprovider/nomad_global.INI
 
 # Start M-APISERVER service
 #exec /usr/local/bin/m-apiserver up -bind="${CONTAINER_IP_ADDR}" 1>&2
